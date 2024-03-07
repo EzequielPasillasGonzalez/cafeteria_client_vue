b1caf4fc8e8f1ec8e9930074fee15e2f97eba72f
Ezequiel <ingepasillasg@gmail.com>
2024-03-07 12:12:52 -0600

    se cambia la fongiracion para produccion

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/https://cafeteria-vue-client.onrender.com/'
+  : '/'
 })
 
 // module.exports = {
