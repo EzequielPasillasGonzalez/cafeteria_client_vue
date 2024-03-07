4691ba8e835735f8fd6ee7a628ee7a8fb66d6ad7
Ezequiel <ingepasillasg@gmail.com>
2024-03-07 12:17:04 -0600

    fix2

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/https://cafeteria-vue-client.onrender.com/'
   : '/'
-})
-
-// module.exports = {
-//   mode: 'production'
-// }
+}
